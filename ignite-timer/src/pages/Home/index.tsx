727b9a6b6b2f2afd9089655c1ea3b96e35fcdbbd
orlandovpjunior <orlando.virginio.penha.junior@ccc.ufcg.edu.br>
2025-04-21 09:16:51 -0300

    feat: implementa pagina de historico com tabelas

--- ignite-timer/src/pages/Home/index.tsx
+++ ignite-timer/src/pages/Home/index.tsx
@@ -1,5 +1,5 @@
 import { Play } from "phosphor-react";
-import { CountdownContainer, FormContainer, HomeContainer, Separator } from "./styles";
+import { CountdownContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StartCountdownButton, TaskInput } from "./styles";
 
 
 export function Home() {
@@ -8,9 +8,26 @@ export function Home() {
             <form action="task">
                 <FormContainer>
                     <label htmlFor="">Vou trabalhar em</label>
-                    <input id="task" />
+                    <TaskInput 
+                        id="task" 
+                        placeholder="Dê um nome para o seu projeto" 
+                        list="taskSuggestions"
+                    />
+                    <datalist id="taskSuggestions">
+                        <option value="Projeto 1" />
+                        <option value="Projeto 2" />
+                        <option value="Projeto 3" />
+
+                    </datalist>
                     <label htmlFor="">durante</label>
-                    <input type="number" id="minutesAmount" />
+                    <MinutesAmountInput 
+                        type="number" 
+                        id="minutesAmount" 
+                        placeholder="00"
+                        step={5}
+                        min={5}
+                        max={60}
+                    />
                     <span>minutos.</span>
                 </FormContainer>
 
@@ -21,10 +38,10 @@ export function Home() {
                     <span>0</span>
                     <span>0</span>
                 </CountdownContainer>
-                <button type="submit">
+                <StartCountdownButton disabled type="submit">
                     <Play size={24} />
                     Começar
-                </button>
+                </StartCountdownButton>
             </form>
         </HomeContainer>
     );
