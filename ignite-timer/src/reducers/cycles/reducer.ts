128baebc9047c862896bbd7e4307cc98e89a31a8
orlandovpjunior <orlando.virginio.penha.junior@ccc.ufcg.edu.br>
2025-04-27 20:28:43 -0300

    refactor: adiciona biblioteca immer e melhora legibilidade do reducer

---
+++ ignite-timer/src/reducers/cycles/reducer.ts
@@ -0,0 +1,54 @@
+import { ActionTypes } from "./actions"
+import {produce} from 'immer'
+ 
+interface cyclesState{
+    cycles: Cycle[]
+    activeCycleId: string | null
+}
+
+export interface Cycle {
+    id: string,
+    task: string,
+    minutesAmount: number,
+    startDate: Date,
+    interruptedDate?: Date,
+    finishedDate?: Date,
+}
+
+export function cyclesReducer(state: cyclesState, action: any) {
+
+        if(action.type == ActionTypes.ADD_NEW_CYCLE){
+            return produce(state, (draft) => {
+                draft.cycles.push(action.payload.newCycle)
+                draft.activeCycleId = action.payload.newCycle.id
+            })
+        }
+        if (action.type == ActionTypes.INTERRUPT_CURRENT_CYCLE) {
+
+            const currentCycleIndex = state.cycles.findIndex(cycle => {
+                return cycle.id == state.activeCycleId
+            })
+
+            if (currentCycleIndex < 0) {
+                return state
+            }
+            return produce(state, (draft) => {
+                draft.activeCycleId = null
+                draft.cycles[currentCycleIndex].interruptedDate = new Date()
+            })
+        }
+        if (action.type == ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED){
+            const currentCycleIndex = state.cycles.findIndex(cycle => {
+                return cycle.id == state.activeCycleId
+            })
+
+            if (currentCycleIndex < 0) {
+                return state
+            }
+            return produce(state, (draft) => {
+                draft.activeCycleId = null
+                draft.cycles[currentCycleIndex].finishedDate = new Date()
+            })
+        }
+        return state
+        }
